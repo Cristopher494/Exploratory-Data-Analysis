a2d0e993c52a33cb4cf44d19f80e06116afcb3bc
Cristopher Rojas <crrojasgo@gmail.com>
2024-04-11 02:47:58 +0200

    Updated project

--- src/app.py
+++ src/app.py
@@ -1,4 +1,47 @@
-from utils import db_connect
-engine = db_connect()
+import pandas as pd
+import matplotlib.pyplot as plt 
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.feature_selection import f_classif, SelectKBest
+from sklearn.preprocessing import MinMaxScaler
 
-# your code here
+total_data = pd.read_csv("https://raw.githubusercontent.com/4GeeksAcademy/data-preprocessing-project-tutorial/main/AB_NYC_2019.csv")
+total_data.to_csv("./data/raw/total_data.csv", index = False)
+
+total_data.drop(["id", "name", "last_review", "host_name", "reviews_per_month"], axis = 1, inplace = True)
+
+total_data = total_data.drop(total_data.index[total_data["minimum_nights"] > 11])
+
+total_data = total_data.drop(total_data.index[total_data["calculated_host_listings_count"] > 4])
+
+
+num_variables = ["neighbourhood_group", "room_type", "minimum_nights", "number_of_reviews", "calculated_host_listings_count", "availability_365"]
+
+X = total_data.drop("price", axis = 1)[num_variables]
+y = total_data["price"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+
+
+scaler = StandardScaler()
+scaler.fit(X_train)
+
+X_train_norm = scaler.transform(X_train)
+X_train_norm = pd.DataFrame(X_train_norm, index = X_train.index, columns = num_variables)
+
+X_test_norm = scaler.transform(X_test)
+X_test_norm = pd.DataFrame(X_test_norm, index = X_test.index, columns = num_variables)
+
+
+selection_model = SelectKBest(f_classif, k = 5)
+selection_model.fit(X_train, y_train)
+ix = selection_model.get_support()
+X_train_sel = pd.DataFrame(selection_model.transform(X_train), columns = X_train.columns.values[ix])
+X_test_sel = pd.DataFrame(selection_model.transform(X_test), columns = X_test.columns.values[ix])
+
+X_train_sel["price"] = list(y_train)
+X_test_sel["price"] = list(y_test)
+
+X_train_sel.to_csv("../data/processed/clean_train.csv", index = False)
+X_test_sel.to_csv("../data/processed/clean_test.csv", index = False)
